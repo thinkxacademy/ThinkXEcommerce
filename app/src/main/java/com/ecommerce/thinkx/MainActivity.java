f12ebba063a8cbe345982c4ac0b1b0e6aef81c61
Vishal <tonyvr53155@gmail.com>
2021-02-24 14:55:55 +0530

    Initial commit

--- app/src/main/java/com/ecommerce/thinkx/MainActivity.java
+++ app/src/main/java/com/ecommerce/thinkx/MainActivity.java
@@ -1,14 +1,71 @@
 package com.ecommerce.thinkx;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextInputLayout mEmail,mPassword;
+    Button mLogin,mRegister;
+    private FirebaseAuth mAuth;
+    String email,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Bind Xml Elements
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
+        mLogin = findViewById(R.id.log_in);
+        mRegister = findViewById(R.id.register);
+        mLogin.setOnClickListener(this);
+        mRegister.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.log_in:
+                loginToFirebase();
+                break;
+            case R.id.register:
+                email = mEmail.getEditText().getText().toString();
+                password = mPassword.getEditText().getText().toString();
+                registerToFirebase();
+                break;
+        }
+    }
+
+    private void registerToFirebase() {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        Toast.makeText(MainActivity.this, "User Registered!!",
+                                Toast.LENGTH_SHORT).show();
+                        //updateUI(user);
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(MainActivity.this, task.getException().toString(),
+                                Toast.LENGTH_SHORT).show();
+                        //updateUI(null);
+                    }
+                });
+    }
+
+    private void loginToFirebase() {
     }
 }
